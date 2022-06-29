2bb04c160317fe36160ec26380a6c5ff86c28e87
Priyanka2004Pinky <106673909+Priyanka2004Pinky@users.noreply.github.com>
2022-06-29 16:39:06 +0530

    Add files via upload

---
+++ ArrayInput.java
@@ -0,0 +1,45 @@
+//import java.util.*;
+/* class ArrayInput
+{
+	public static void main(String[] args) 
+	{
+		int i;
+		System.out.println("String Length");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();            // string length = n
+		int a[] = new int[n];			//Array of length n 
+		for(i=0;i<=n-1;i++)
+		{
+			System.out.println("Enter marks");
+		    a[i]= sc.nextInt();
+		}
+		for(i=0;i<=n-1;i++)
+		{
+			System.out.print(a[i]+" ");
+		}
+	}
+}
+*/
+/*
+
+class ArrayInput
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("String Length");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();            // string length = n
+		int a[] = new int[n];			//Array of length n 
+		System.out.println("String Length =" +" " + a.length);
+		for(int i=0;i<=a.length-1;i++)
+		{
+			System.out.println("Enter marks");
+		    a[i]= sc.nextInt();
+		}
+		for(int i=0;i<=a.length-1;i++)
+		{
+			System.out.print(a[i]+" ");
+		}
+	}
+}
+*/
